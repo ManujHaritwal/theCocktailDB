707abb3f585048839988b7e556efa5f26e44007e
Manuj Haritwal <haritwalmanuj99@gmail.com>
2022-11-16 15:33:52 +0530

    para fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,13 +5,13 @@ const About = () => {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Quasi quis temporibus nisi ducimus laudantium voluptatem.
-         Inventore deleniti explicabo reprehenderit blanditiis natus nam minus voluptatum aliquid vero illum, 
-         autem accusantium sequi.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Quasi quis temporibus nisi ducimus laudantium voluptatem.
-         Inventore deleniti explicabo reprehenderit blanditiis natus nam minus voluptatum aliquid vero illum, 
-         autem accusantium sequi.
+        The company itself is a very successful company.
+        As if we were only leading the pleasure of the praisers at times.
+        I will explain to the inventor of gentleness, he will criticize the one who was born with flattery,
+        because he has less pleasures, but to follow the accusers.
+        As if we were only leading the pleasure of the praisers at times.
+        I will explain to the inventor of gentleness, he will reproach the one born with flattery,
+        because he is indeed something less pleasures, but to follow the accusers.
       </p>
     </section>
   )
